51ae96c5e2e4bf3d3a32af9c0267d97e1758876b
Zukhra <zuhruschka@mail.ru>
2022-10-26 20:58:10 +0400

    EX1

---
+++ EX1/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа//
+Console.WriteLine("Введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа - "+stringNumber[1]);
